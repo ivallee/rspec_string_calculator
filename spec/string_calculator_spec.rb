84da39d16305314d4e2d805d18f033dc6ce8a795
Isaac Vallee <isaacvallee@gmail.com>
2017-10-14 20:32:43 +0000

    add contexts

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,6 +13,11 @@ describe StringCalculator do
           expect(StringCalculator.add('4')).to eql(4)
         end
       end
+      context 'given \'10\'' do
+        it 'returns 10' do
+          expect(StringCalculator.add('10')).to eql(10)
+        end
+      end
     end
     context 'given \'2,4\'' do
       it 'returns 6' do
