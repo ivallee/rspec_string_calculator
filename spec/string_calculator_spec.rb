402988c21c462d0862addcabcda48c3d609af8ba
Isaac Vallee <isaacvallee@gmail.com>
2017-10-14 20:31:59 +0000

    inplement addition in .add

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,6 +2,18 @@ require 'string_calculator'
 
 describe StringCalculator do
   describe '.add' do
+    context 'given an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add('0')).to eql(0)
+      end
+    end
+    context 'given single numbers' do
+      context 'given \'4\'' do
+        it 'returns 4' do
+          expect(StringCalculator.add('4')).to eql(4)
+        end
+      end
+    end
     context 'given \'2,4\'' do
       it 'returns 6' do
         expect(StringCalculator.add('2,4')).to eql(6)
